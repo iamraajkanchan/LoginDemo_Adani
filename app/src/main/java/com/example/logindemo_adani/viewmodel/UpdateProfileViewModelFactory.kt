253748b8da128a6136a1a85fa0ab213f14c77e40
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-05-25 21:55:20 +0530

    Updated on 25th March 2022

--- app/src/main/java/com/example/logindemo_adani/viewmodel/UpdateProfileViewModelFactory.kt
+++ app/src/main/java/com/example/logindemo_adani/viewmodel/UpdateProfileViewModelFactory.kt
@@ -3,11 +3,16 @@ package com.example.logindemo_adani.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.logindemo_adani.repositories.UpdateProfileRepository
+import java.lang.IllegalArgumentException
 
 class UpdateProfileViewModelFactory(private val updateProfileRepository : UpdateProfileRepository) : ViewModelProvider.Factory
 {
     override fun <T : ViewModel> create(modelClass : Class<T>) : T
     {
-        return UpdateProfileViewModel(updateProfileRepository) as T
+        if (modelClass.isAssignableFrom(UpdateProfileViewModel::class.java))
+        {
+            return UpdateProfileViewModel(updateProfileRepository) as T
+        }
+        throw IllegalArgumentException("Local and anonymous classes can not be ViewModels")
     }
 }
